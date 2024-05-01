79f57ee0a6cc6f36d36042d110bfb93437b8e6d0
onlinesmit <onlinesmitg@gmail.com>
2024-05-01 20:50:05 +0530

    .

--- codedamm-java-dsa-practice/decTobin.java
+++ codedamm-java-dsa-practice/decTobin.java
@@ -4,11 +4,11 @@ public class decTobin {
   public static void main(String[] args) {
     
     int n =10;
-    int rev =0;
+    String rev ="";
     while (n>0) {
       int rem = n%2;
-      rev = rev*10 + rem;
       n =n/2;
+      rev = rem +rev;
     }
   }
   }
